e2431eebf2062ce4cf042b4ee18eef3bc5c1d2c3
VettoriKev <89916190+VettoriKev@users.noreply.github.com>
2023-10-06 19:56:36 -0300

    ApiV1

---
+++ ApiRest.py
@@ -0,0 +1,23 @@
+# Importa Flask
+from flask import Flask
+
+# Crea una instancia de Flask
+app = Flask(__name__)
+
+# Define una ruta para el recurso `datos`
+@app.route("/api/datos", methods=["POST"])
+
+# Define la función que se ejecutará cuando se reciba una petición HTTP al recurso `datos`
+def datos():
+    # Obtén los datos de la petición
+    data = request.json()
+
+    # Procesa los datos
+    temperatura = data["temperatura"]
+    humedad = data["humedad"]
+
+    # Devuelve una respuesta
+    return jsonify({"temperatura": temperatura, "humedad": humedad})
+
+# Ejecuta la aplicación
+app.run()
